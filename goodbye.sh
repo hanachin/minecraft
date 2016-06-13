c314b01e3c2d822c0d5c0dfd874d05f15adac71c
Seiei Miyagi <hanachin@gmail.com>
2016-06-13 20:14:17 +0900

    wait before shut down

--- goodbye.sh
+++ goodbye.sh
@@ -5,7 +5,10 @@ source /home/mc/.envrc
 date >> /tmp/goodbye
 
 if [ -n "$(/sbin/service minecraft command 'list' | grep 'There are 0')" ]; then
-  echo 'shut down server'
-  /sbin/service minecraft stop
-  curl -H "x-api-key: $X_API_KEY" -X DELETE $ENDPOINT_URL
+  sleep 180
+  if [ -n "$(/sbin/service minecraft command 'list' | grep 'There are 0')" ]; then
+    echo 'shut down server'
+    /sbin/service minecraft stop
+    curl -H "x-api-key: $X_API_KEY" -X DELETE $ENDPOINT_URL
+  fi
 fi
