b77161407d14cdd34abf364a666a8daf819f9459
Seiei Miyagi <hanachin@gmail.com>
2016-06-19 07:47:00 +0900

    post tweet before stop server

--- goodbye.sh
+++ goodbye.sh
@@ -10,6 +10,7 @@ if [ -n "$(/sbin/service minecraft command 'list' | grep 'There are 0')" ]; then
   if [ -n "$(/sbin/service minecraft command 'list' | grep 'There are 0')" ]; then
     echo 'shut down server'
     /sbin/service minecraft stop
+    curl -X POST https://maker.ifttt.com/trigger/stop/with/key/$IFTTT_MAKER_KEY
     curl -H "x-api-key: $X_API_KEY" -X DELETE $ENDPOINT_URL
   fi
 fi
