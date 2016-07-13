409c40208e5e0db885220e66c8867a057454a742
Seiei Miyagi <hanachin@gmail.com>
2016-07-14 02:38:37 +0900

    SSH from my home

--- minecraft.tf
+++ minecraft.tf
@@ -29,6 +29,28 @@ resource "aws_ebs_volume" "minecraft" {
   }
 }
 
+variable "my_home_ip" {}
+
+resource "aws_security_group" "ssh_from_home" {
+  name = "ssh from home"
+  description = "ssh from my home ip"
+  vpc_id = "${aws_vpc.minecraft.id}"
+
+  ingress {
+    from_port = 22
+    to_port =  22
+    protocol = "tcp"
+    cidr_blocks = ["${var.my_home_ip}/32"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "minecraft" {
   name = "minecraft server"
   description = "tcp port for minecraft server"
