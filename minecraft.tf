966a67090aa4208ff702f61a9694ee61cb109390
Seiei Miyagi <hanachin@gmail.com>
2016-07-14 02:04:16 +0900

    Open outgoing ports

--- minecraft.tf
+++ minecraft.tf
@@ -40,4 +40,11 @@ resource "aws_security_group" "minecraft" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
