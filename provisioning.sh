6b15a86c03d538176610fbccc9d2cf7eef2c37a4
Seiei Miyagi <hanachin@gmail.com>
2016-07-31 14:36:54 +0900

    Update provisioning.sh, add host arguments

--- provisioning.sh
+++ provisioning.sh
@@ -6,5 +6,11 @@ if [ ! -f minecraft_server.jar ]; then
   curl -LSfs -o minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/1.10/minecraft_server.1.10.jar
 fi
 
+if [ -z $1 ]; then
+  host='minecraft'
+else
+  host=$1
+fi
+
 bundle check --path=.bundle || bundle install --path=.bundle
-bundle exec serverkit apply --log-level INFO --hosts minecraft --variables=variables.yml recipe.yml.erb
+bundle exec serverkit apply --log-level INFO --hosts $host --variables=variables.yml recipe.yml.erb
