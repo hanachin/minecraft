5cfaf22dde383f5e833e1b40de154824a155239a
Seiei Miyagi <hanachin@gmail.com>
2016-09-18 16:19:43 +0900

    Fix mc bot cron

--- files/mc_twitter_bot/wakeup.sh
+++ files/mc_twitter_bot/wakeup.sh
@@ -3,5 +3,5 @@
 set -ex
 
 if [ -z "$(/sbin/service mc_twitter_bot status | grep 'is running')" ]; then
-  /sbin/service mc_twitter_bot start
+  sudo /sbin/service mc_twitter_bot start >> /home/ec2-user/wakeup.log
 fi
