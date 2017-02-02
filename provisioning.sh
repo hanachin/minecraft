d406a9e43b84986fa7ce9cbe1681073ee1328d71
Seiei Miyagi <hanachin@gmail.com>
2017-02-02 23:34:47 +0900

    Update recipe, TODO: restart minecraft server when upgrade server done

--- provisioning.sh
+++ provisioning.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-if [ ! -f files/minecraft_server.jar ]; then
-  curl -LSfs -o files/minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/1.11/minecraft_server.1.11.jar
+if [ ! -f files/minecraft_server.1.11.2.jar ]; then
+  curl -LSfs -o files/minecraft_server.1.11.2.jar https://s3.amazonaws.com/Minecraft.Download/versions/1.11.2/minecraft_server.1.11.2.jar
 fi
 
 if [ -z $1 ]; then
